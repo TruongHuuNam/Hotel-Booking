7449e93891209fa5582001a8978b23a7dd35b517
Hữu Nam <namcute0910@gmail.com>
2024-11-13 21:26:50 +0700

    First commit

---
+++ sendEmail.php
@@ -0,0 +1,53 @@
+<?php
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Lấy dữ liệu từ form
+    $customerName = $_POST["customerName"];
+    $customerNumber = $_POST["customerNumber"];
+    $customerEmail = $_POST["customerEmail"];
+    $serviceRequest = $_POST["serviceRequest"];
+
+    // Khởi tạo PHPMailer
+    $mail = new PHPMailer(true);
+
+    try {
+        // Cấu hình SMTP
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'nguyenloi1442@gmail.com';
+        $mail->Password   = 'reoyrhwlswgikgmj';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = 465;
+
+        // Thiết lập các thông tin người gửi và người nhận
+        $mail->setFrom('nguyenloi1442@gmail.com');
+        $mail->addAddress('nguyenthanhloi1442@gmail.com');
+        
+        $encodedSubject = mb_encode_mimeheader('Yêu cầu dịch vụ từ ' . $customerName, 'UTF-8', 'Q');
+
+        // Thiết lập tiêu đề và nội dung email
+        $mail->isHTML(true);
+        $mail->Subject = $encodedSubject;
+        $mail->Body    = 'Họ và tên: ' . $customerName . '<br>Số điện thoại:' . $customerNumber . '<br>Email: ' . $customerEmail . '<br>Yêu cầu dịch vụ: ' . $serviceRequest;
+
+        // Gửi email
+        $mail->send();
+        echo '<p>Message has been sent.</p>';
+        echo '<script>
+                setTimeout(function() {
+                    window.location.href = "service.php";
+                }, 2000);
+              </script>';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+?>
